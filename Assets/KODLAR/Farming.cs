e2ad49a012d498463ac7c21300809bc671780489
MUHAMMED RAŞİD KÜÇÜKYILMAZ <kanaryaskl66@gmail.com>
2022-07-12 14:05:01 +0300

    Updated

--- Assets/KODLAR/Farming.cs
+++ Assets/KODLAR/Farming.cs
@@ -1,37 +1,83 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Farming : MonoBehaviour
 {
-    public GameObject Level_0_Prefab;
+    public List<GameObject> Seedlings;
+    public List<GameObject> Carrots;
+    public List<GameObject> Virgos;
+    private bool Growing;
+    public GameObject EkmeTusu;
 
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Seedlings.Add(transform.GetChild(i).GetChild(1).gameObject);
+            Carrots.Add(transform.GetChild(i).GetChild(2).gameObject);
+            Virgos.Add(transform.GetChild(i).GetChild(0).gameObject);
+
+            if (i == transform.childCount - 1) break;
+        }
+    }
     void Start()
     {
-        
+
     }
     private void Update()
     {
-        FarmLevels();
-    }
-
-    public void FarmLevels()
-    {
 
     }
+
 
     private void OnTriggerStay(Collider col)
     {
-        if (col.tag == "Player")
+
+        if (col.CompareTag("Player") && !Growing)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            EkmeTusu.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (EkmeTusu.activeInHierarchy)
+        {
+            EkmeTusu.SetActive(false);
+        }
+    }
+
+    public void Farmer(bool seedling)
+    {
+
+        if (seedling && !Growing)
+        {
+            for (int i = 0; i < transform.childCount; i++)
             {
-                for (int i = 0; i < transform.childCount; i++)
-                {
-                    //Instantiate(Level_0_Prefab, transform);
-                    Debug.Log(i);
-                }
+                Seedlings[i].SetActive(true);
+                Growing = true;
+                StartCoroutine(Growings());
+                EkmeTusu.SetActive(false);
             }
         }
+    }
+
+    IEnumerator Growings()
+    {
+        yield return new WaitForSeconds(5);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Seedlings[i].SetActive(false);
+            Carrots[i].SetActive(true);
+        }
+        yield return new WaitForSeconds(5);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Carrots[i].SetActive(false);
+            Virgos[i].SetActive(true);
+            StopCoroutine(Growings());
+        }
+        
     }
 }
